a1a476cd3c8d9c0fa0c8c4ea522543051232e33e
fiveseven808 <yosh.five7@gmail.com>
2017-03-20 11:52:03 -1000

    tabs to spaces
    lol wth, why were tabs used???
    
    also, it works now

--- geo_wifi.py
+++ geo_wifi.py
@@ -14,14 +14,14 @@ import requests
 import os
 
 try:
-	with open(os.path.expanduser("api.key"), 'r') as f:
-		contents = f.read()
+    with open(os.path.expanduser("api.key"), 'r') as f:
+        contents = f.read()
 except IOError:
-	print "api.key not found... Quitting"
-	sys.exit()
+    print "api.key not found... Quitting"
+    sys.exit()
 KEY = contents.split('\r\n') # Try and parse out microsoft created files
 if len(KEY) <= 1: # If it doesn't parse out properly
-	KEY = contents.split('\n') # Try and parse the "normal" way
+    KEY = contents.split('\n') # Try and parse the "normal" way
 KEY = KEY[0]
 CMD_CHROMEDRIVER = "/usr/local/bin/chromedriver"
 
@@ -30,94 +30,162 @@ CMD_CHROMEDRIVER = "/usr/local/bin/chromedriver"
 #
 class GeoWifi():
 
-	HEADERS = { 'Content-Type' : 'application/json' }
+    HEADERS = { 'Content-Type' : 'application/json' }
 
-	def request(self, key, addr1, addr2 ):
-		url = "https://www.googleapis.com/geolocation/v1/geolocate?key=" + key
-		print url
-		text = self.buildJson( addr1, addr2 )
-		#print text #this is a json
-		#res = requests.post(url, json=text) # for some reason this isn't very accurate...
-		curlurl = str("curl -H \"Content-Type: application/json\" -H \"Accept: application/json\" -X POST -d '"+str(text)+"' " + str(url))
-		print curlurl
-		res = os.popen(curlurl).read()
-		print res
-		#req = urllib2.Request(url, text, self.HEADERS)
-		res = json.loads(res)
-		print res
-		#res = urllib2.urlopen(req)
-		body = res
-		return self.parseResponse(body)
+    def request(self, key, addr1, addr2 ):
+        url = "https://www.googleapis.com/geolocation/v1/geolocate?key=" + key
+        print url
+        text = self.buildJson( addr1, addr2 )
+        #print text #this is a json
+        #res = requests.post(url, json=text) # for some reason this isn't very accurate...
+        curlurl = str("curl -H \"Content-Type: application/json\" -H \"Accept: application/json\" -X POST -d '"+str(text)+"' " + str(url))
+        #print curlurl
+        res = os.popen(curlurl).read()
+        print res
+        #req = urllib2.Request(url, text, self.HEADERS)
+        res = json.loads(res)
+        #print res
+        #res = urllib2.urlopen(req)
+        body = res
+        return self.parseResponse(body)
 
-	def buildJson(self, addr1, addr2):
-		obj = {}
-		obj[ "wifiAccessPoints" ] = self.buildAddressList(addr1, addr2)
-		text = json.dumps(obj)
-		return text
+    def request_new(self, key, mac_array, p_array ):
+        url = "https://www.googleapis.com/geolocation/v1/geolocate?key=" + key
+        print url
+        text = self.buildJson_array(mac_array, p_array)
+        #print text #this is a json
+        #res = requests.post(url, json=text) # for some reason this isn't very accurate...
+        curlurl = str("curl -H \"Content-Type: application/json\" -H \"Accept: application/json\" -X POST -d '"+str(text)+"' " + str(url))
+        #print curlurl
+        res = os.popen(curlurl).read()
+        print res
+        #req = urllib2.Request(url, text, self.HEADERS)
+        res = json.loads(res)
+        #print res
+        #res = urllib2.urlopen(req)
+        body = res
+        return self.parseResponse(body)
 
-	def buildAddressList(self, addr1, addr2):
-		list = []
-		list.append( self.buildAddress(addr1) )
-		list.append( self.buildAddress(addr2) )
-		return list
+    def buildJson(self, addr1, addr2):
+        obj = {}
+        obj[ "wifiAccessPoints" ] = self.buildAddressList(addr1, addr2)
+        text = json.dumps(obj)
+        return text
 
-	def buildAddress(self, addr):
-		dict = { "macAddress": addr }
-		return dict
+    def buildJson_array(self, mac_array, p_array):
+        obj = {}
+        obj[ "wifiAccessPoints" ] = self.buildAddressList2(mac_array, p_array)
+        text = json.dumps(obj)
+        return text
 
-	def parseResponse(self, res):
-		#obj = json.loads(res)
-		obj = res
-		if obj["location"] is None:
-			print res
-			return None
-		if obj["location"]["lat"] is None:
-			print res
-			return None
-		if obj["location"]["lng"] is None:
-			print res
-			return None
-		if obj["accuracy"] is None:
-			accuracy = 0
-		else:
-			accuracy = obj["accuracy"]
-		ret = {}
-		ret["lat"] = obj["location"]["lat"]
-		ret["lng"] = obj["location"]["lng"]
-		ret["accuracy"] = accuracy
-		return ret
+
+    def buildAddressList(self, addr1, addr2):
+        list = []
+        list.append( self.buildAddress(addr1) )
+        list.append( self.buildAddress(addr2) )
+        return list
+
+    def buildAddressList2(self, mac_array, p_array):
+        list = []
+        for i,m in enumerate(mac_array):
+            if len(m) == 0:
+                break
+            list.append(self.buildAddress(m,p_array[i]))
+        #list.append( self.buildAddress(addr1) )
+        #list.append( self.buildAddress(addr2) )
+        return list
+
+    def buildAddress(self, addr,power):
+        dict = { "macAddress": addr, "signalStrength": power }
+        return dict
+
+    def parseResponse(self, res):
+        #obj = json.loads(res)
+        obj = res
+        if obj["location"] is None:
+            print res
+            return None
+        if obj["location"]["lat"] is None:
+            print res
+            return None
+        if obj["location"]["lng"] is None:
+            print res
+            return None
+        if obj["accuracy"] is None:
+            accuracy = 0
+        else:
+            accuracy = obj["accuracy"]
+        ret = {}
+        ret["lat"] = obj["location"]["lat"]
+        ret["lng"] = obj["location"]["lng"]
+        ret["accuracy"] = accuracy
+        return ret
 
 # class end
 
 def openChrome(lat, lng):
-	url = "https://maps.google.com/maps?q=" + str(lat) + "," + str(lng) + "&z=12"
-	#driver = webdriver.Chrome( CMD_CHROMEDRIVER )
-	#driver.get(url);
-	print("This is the URL to visit:")
-	print url
+    url = "https://maps.google.com/maps?q=" + str(lat) + "," + str(lng) + "&z=12"
+    #driver = webdriver.Chrome( CMD_CHROMEDRIVER )
+    #driver.get(url);
+    print("This is the URL to visit:")
+    print url
 
-# main
-args = sys.argv
-argc = len(args)
-if (len(args) < 3):
-	print 'Usage: python %s mac_addr_1 mac_addr_2' % args[0]
-	exit()
+def locateself(debug_arg):
+    if debug_arg == 1:
+        os.popen("cat wifilist | grep WPA | sort -r -k3 -n > /tmp/wifi")
+        powers = os.popen("cat /tmp/wifi | awk '{print $3}'").read()
+        macs = os.popen("cat /tmp/wifi | awk '{print $1}'").read()
+    else:
+        os.popen("wpa_cli scan")
+        time.sleep(3) # to allow the scan results to populate
+        os.popen("wpa_cli scan_results > /tmp/wifi_all")
+        # Only display WPS enabled points
+        os.popen("cat /tmp/wifi_all | grep WPA | sort -r -k3 -n > /tmp/wifi")
+        powers = os.popen("cat /tmp/wifi | awk '{print $3}'").read()
+        macs = os.popen("cat /tmp/wifi | awk '{print $1}'").read()
+    mac_array = macs.split('\n')
+    p_array = powers.split('\n')
+    geo = GeoWifi()
+    res = geo.request_new( KEY, mac_array, p_array )
+    if res is None:
+        exit()
+    print str(res["lat"]) + " " + str(res["lng"]) + " "+ str(res["accuracy"])
+    openChrome( res["lat"], res["lng"] )
 
-geo = GeoWifi()
-res = geo.request( KEY, args[1], args[2] )
-if res is None:
-	exit()
+if __name__ == "__main__":
+    # main
+    args = sys.argv
+    argc = len(args)
+    self_arg = 0
+    debug_arg = 0
+    for arg in sys.argv:
+        if arg == '-self':
+            self_arg = 1
+        if arg == '-d':
+            debug_arg = 1
 
-print str(res["lat"]) + " " + str(res["lng"]) + " "+ str(res["accuracy"])
-openChrome( res["lat"], res["lng"] )
-"""
-print "Press CTRL+C to quit"
-try:
-	# endless loop
-	while True:
-		time.sleep(1)
-except KeyboardInterrupt:
-	# exit the loop, if key interrupt
-	pass
-"""
-# end
+    if (len(args) < 3 and self_arg == 0):
+        print 'Usage: python %s mac_addr_1 mac_addr_2' % args[0]
+        exit()
+    elif self_arg == 1:
+        locateself(debug_arg)
+        exit()
+
+    geo = GeoWifi()
+    res = geo.request( KEY, args[1], args[2] )
+    if res is None:
+        exit()
+
+    print str(res["lat"]) + " " + str(res["lng"]) + " "+ str(res["accuracy"])
+    openChrome( res["lat"], res["lng"] )
+    """
+    print "Press CTRL+C to quit"
+    try:
+        # endless loop
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        # exit the loop, if key interrupt
+        pass
+    """
+    # end
